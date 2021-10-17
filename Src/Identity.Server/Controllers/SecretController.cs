ea520e7aa5cead92a8610ac8172ff15be705fa9f
MassoudKargar <MassoudKargar@outlook.com>
2021-10-17 14:15:02 +0330

    Create Server Services

---
+++ Src/Identity.Server/Controllers/SecretController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Server.Controllers
+{
+    public class SecretController : Controller
+    {
+        [Authorize]
+        public string Index()
+        {
+            return "secret message";
+        }
+    }
+}
