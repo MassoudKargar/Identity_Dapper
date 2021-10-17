a4f959b69ff5397299fc6d3675b4d8f658a4fd76
MassoudKargar <MassoudKargar@outlook.com>
2021-10-17 13:59:22 +0330

    Remove All Library

--- Src/Identity.Server/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Identity.Server
-{
-    public static class Constants
-    {
-        public const string Issuer = Audiance;
-        public const string Audiance = "https://localhost:6001/";
-        public const string Secret = "not_too_short_secret_otherwise_it_might_error";
-    }
-}
